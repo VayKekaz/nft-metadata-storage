6b40f3f32c9032d3c1c650bbeea80cb4187189ca
alyssa moon <themoongg@yandex.ru>
2022-04-15 12:20:05 +0300

    all metadata templates

---
+++ createMetadata.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+function doNothing() {
+}
+
+function createMetadata(imageFilenames) {
+    console.log(imageFilenames);
+    for (const [index, imgFilename] of imageFilenames.entries()) {
+        const metadataFilename = `${index}`.padEnd(64, "0") + ".json";
+        const metadataContent = {
+            name: imgFilename,
+            description: `Original artwork of Ketari with ${imgFilename} fictional character.`,
+            image: `https://raw.githubusercontent.com/VayKekaz/nft-metadata-storage/main/image/${imgFilename}`,
+            attributes: [
+                {
+                    trait_type: "character",
+                    value: `${imgFilename}`,
+                },
+            ],
+        };
+        fs.writeFile(`./metadata/${metadataFilename}`, JSON.stringify(metadataContent), function (err, result) {
+            if (err) console.log("error", err);
+        });
+    }
+}
+
+fs.readdir("./image", (e, files) => {
+    createMetadata(files);
+});
